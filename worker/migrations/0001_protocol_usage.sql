0e1526715c9c2795a5bbda7ffe6f24e3512631a3
Bartosz Fenski <fenio@debian.org>
2026-07-31 10:33:17 +0200

    telemetry: report sharing protocol adoption

---
+++ worker/migrations/0001_protocol_usage.sql
@@ -0,0 +1,4 @@
+ALTER TABLE telemetry ADD COLUMN smb_shares INTEGER;
+ALTER TABLE telemetry ADD COLUMN nfs_exports INTEGER;
+ALTER TABLE telemetry ADD COLUMN iscsi_luns INTEGER;
+ALTER TABLE telemetry ADD COLUMN nvmeof_namespaces INTEGER;
